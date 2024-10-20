628156cf405c6c5447279b9598c95bc8bd97c7a7
jc-cardosoriveroll <jc@cardosoriveroll.com>
2024-10-19 19:57:51 -0700

    ftp

--- wwwroot/demos/3_TicTacToe/scripts/script.js
+++ wwwroot/demos/3_TicTacToe/scripts/script.js
@@ -63,8 +63,8 @@ function pick(pos){
 }
 
 function updateUI(){
+    alert("update");
     for (let i = 0; i < window.game.history; i++) {
-        alert(JSON.stringify(window.game.history[i]));
         /*
         move = game.history[i];
         cell = document.getElementById(move.pos);
