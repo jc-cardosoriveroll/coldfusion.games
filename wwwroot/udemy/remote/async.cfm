0f221b0885a0b98501940d0f35289ed8d18feaba
jc-cardosoriveroll <jc@cardosoriveroll.com>
2024-10-25 11:02:35 -0700

    ftp

--- wwwroot/udemy/remote/async.cfm
+++ wwwroot/udemy/remote/async.cfm
@@ -1,1 +1,4 @@
-<!--- temp --->
+<cfparam name="url.letter"> <!--- new guess --->
+<!--- Run function to determin which positions to show --->
+<cfset application.game.playGame(letter=url.letter)>
+<cfoutput>#serializeJSON(application.game)#</cfoutput>
--- wwwroot/udemy/scripts/script.js
+++ wwwroot/udemy/scripts/script.js
@@ -4,7 +4,27 @@ $(document).ready(function() {
     $( ".pick" ).click(function(){
         /* get values from clicked element */
         var letter = $(this).attr("letter");
-        alert("your letter is: " + letter);
+
+
+        /* Call Async URL with data */
+        $.get( "remote/async.cfm?letter=" + letter, function( data ) {
+            /* ----------------------------------------------- */
+                /* expect a data response like: 
+                {"data":
+                    {"hits":["a"],"word":"India","category":"Countries","wonOrLost":"None"
+                    ,"miss":["b"],"gameOver":false,"hangImg":"images/step1.png","level":"1"
+                    ,"show":["images/0_hires.png","images/0_hires.png","images/0_hires.png","images/0_hires.png","images/a_hires.png"] }}             
+            /* ----------------------------------------------- */
+
+            /* always good practice to debug in console */
+            console.log(data);
+            var game = $.parseJSON(data);
+            console.log(game);
+        });
+
+
+        
+
     });
 
 });
