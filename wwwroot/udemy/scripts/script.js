4da3135a6f503b1b5ee8268563426845cf0a6bd9
jc-cardosoriveroll <jc@cardosoriveroll.com>
2024-10-24 19:25:45 -0700

    ftp

--- wwwroot/udemy/scripts/script.js
+++ wwwroot/udemy/scripts/script.js
@@ -1,1 +1,11 @@
 // * script *//
+$(document).ready(function(){
+
+
+    $(".letter").click(function(){
+        var letter = $(this).attr("letter")
+        alert("Hola! picaste la letra..." + letter);
+    });
+
+});
+
